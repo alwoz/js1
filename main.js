bf12d5a46bae6e69748a07429035b50042f0a931
owozniak <aleksander.wozniak0@gmail.com>
2023-04-20 07:59:33 +0200

    zadania 23-26

---
+++ main.js
@@ -0,0 +1,10 @@
+let firstName = 'Olek';
+let lastName = 'Wozniak';
+
+console.log('Uwielbiam JavaScript!');
+
+let array = [1,2,3];
+let num1 = 1;
+let num2 = 2;
+let num3 = 3;
+let array1 = [num1,num2,num3];
